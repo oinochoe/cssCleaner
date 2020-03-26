9210c25d9775201485bfa9ce442a249406646c07
oinochoe <oinochoe@naver.com>
2020-03-26 19:45:03 +0900

    csscompact

---
+++ index.js
@@ -0,0 +1,68 @@
+const CleanCSS = require("clean-css");
+const fs = require("fs");
+const origin = "origin";
+const out = "out";
+const originSrc = fs
+  .readdirSync(origin)
+  .filter(file => /\.css/.test(file))
+  .map(files => `${origin}/${files}`);
+const outSrc = fs
+  .readdirSync(out)
+  .filter(file => /\.css/.test(file))
+  .map(files => `${out}/${files}`);
+
+const options = {
+  format: {
+    indentBy: 0,
+    breaks: {
+      afterAtRule: false, // controls if a line break comes after an at-rule; e.g. `@charset`; defaults to `false`
+      afterBlockBegins: false, // controls if a line break comes after a block begins; e.g. `@media`; defaults to `false`
+      afterBlockEnds: false, // controls if a line break comes after a block ends, defaults to `false`
+      afterComment: false, // controls if a line break comes after a comment; defaults to `false`
+      afterProperty: false, // controls if a line break comes after a property; defaults to `false`
+      afterRuleBegins: false, // controls if a line break comes after a rule begins; defaults to `false`
+      afterRuleEnds: false, // controls if a line break comes after a rule ends; defaults to `false`
+      beforeBlockEnds: false, // controls if a line break comes before a block ends; defaults to `false`
+      betweenSelectors: true // controls if a lin
+    },
+    spaces: {
+      aroundSelectorRelation: true, // controls if spaces come around selector relations; e.g. `div > a`; defaults to `false`
+      beforeBlockBegins: true, // controls if a space comes before a block begins; e.g. `.block {`; defaults to `false`
+      beforeValue: true // controls if a space comes before a value; e.g. `width: 1rem`; defaults to `false`
+    }
+  }
+};
+
+let css = "";
+
+for (let index = 0; index < originSrc.length; index++) {
+  // after read files
+  readFiles().then(function(resolveData) {
+    console.log(css);
+    // writeFile
+    var minified = new CleanCSS(options).minify(css).then(function(output) {
+      return output.styles;
+    });
+    console.log(minified);
+
+    /* new CleanCSS({ options, returnPromise: true })
+        .minify(originSrc)
+        .then(function(resolveData) {
+          fs.writeFile(outSrc[index], resolveData, (error, data) => {
+            if (error) throw error;
+            console.log("생성 완료");
+          });
+        }); */
+  });
+
+  function readFiles() {
+    return new Promise(function(resolve, reject) {
+      fs.readFile(originSrc[index], "utf8", (error, data) => {
+        if (error) throw error;
+        console.log("읽기 완료");
+        css = data;
+        resolve(css);
+      });
+    });
+  }
+}
